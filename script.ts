10ee4b685b610be1b07d10f846da102b4990ef06
solitaire <fmailiulian@gmail.com>
2025-01-02 00:49:56 +0200

    added function start() and some modfications

--- script.ts
+++ script.ts
@@ -20,32 +20,35 @@ function hash(input: string, saltsize: number, keysize: number, iterations: numb
 
 function prompt(saltsize: number, keysize: number, iterations: number): void {
     rl.question('Enter something to be hashed: ', (input) => {
-        if(input.trim().toLowerCase() === 'exit')
-        {
-            rl.close();
-            return;
-        }
+        if(input.trim().toLowerCase() === 'exit') return rl.close();
         hash(input, saltsize, keysize, iterations);
         prompt(saltsize, keysize, iterations);
     });
 }
 
-rl.question('Enter a salt number (number of bytes): ', (salt) => {
-    rl.question('Enter key number (number of bytes): ', (key) => {
-        rl.question('Enter number of iterations: ', (iter) => {
-            const saltsize = parseInt(salt, 10);
-            const keysize = parseInt(key, 10);
-            const iterations = parseInt(iter, 10);
-
-            if(isNaN(saltsize) || isNaN(keysize) || isNaN(iterations))
-            {
-                console.error('All inputs must be valid numbers.');
-                rl.close();
-                return;
-            }
-
-            console.log(`Ready to hash with ${saltsize} bytes saltsize, ${keysize} bytes keysize and ${iterations} iterations.`);
-            prompt(saltsize, keysize, iterations);
+function start(){
+    rl.question('Enter a salt number (number of bytes): ', (salt) => {
+        if(salt.trim().toLowerCase() === 'exit') return rl.close();
+        rl.question('Enter key number (number of bytes): ', (key) => {
+            if(key.trim().toLowerCase() === 'exit') return rl.close();
+            rl.question('Enter number of iterations: ', (iter) => {
+                if(iter.trim().toLowerCase() === 'exit') return rl.close();
+                const saltsize = parseInt(salt, 10);
+                const keysize = parseInt(key, 10);
+                const iterations = parseInt(iter, 10);
+    
+                if(isNaN(saltsize) || isNaN(keysize) || isNaN(iterations))
+                {
+                    console.error('\x1b[31m%s\x1b[0m', 'All inputs must be valid numbers.');
+                    return start();
+                }
+    
+                console.log(`Ready to hash with ${saltsize} bytes saltsize, ${keysize} bytes keysize and ${iterations} iterations.`);
+                prompt(saltsize, keysize, iterations);
+            });
         });
     });
-});
+}
+
+console.log('\x1b[32m%s\x1b[0m', 'Example: 8 bytes salt, 32 bytes key, 10000 iterations');
+start();
